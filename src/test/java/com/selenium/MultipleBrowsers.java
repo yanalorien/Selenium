3378128e3e16564b49c6926ee77e66cd712d1c66
yanalorien <yankaeva@gmail.com>
2020-11-23 23:42:45 -0500

    switch browsers

--- src/test/java/com/selenium/MultipleBrowsers.java
+++ src/test/java/com/selenium/MultipleBrowsers.java
@@ -7,24 +7,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 class MultipleBrowsers
 {
 	private WebDriver driver;
 	private String title = "SeleniumHQ Browser Automation";
+	private String browser = "edge";
 	
 	@BeforeEach
 	void setup()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
-		driver = new ChromeDriver();
+		switch(browser)
+		{
+			case "chrome" -> {
+				System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+				driver = new ChromeDriver();}
+			case "firefox" -> {
+				System.setProperty("webdriver.gecko.driver", "C:/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();}
+			case "edge" -> {
+				System.setProperty("webdriver.edge.driver", "C:/drivers/msedgedriver.exe");
+				driver = new EdgeDriver();}
+		}
 	}
 	
 	@Test
 	void test()
 	{
 		driver.get("https://www.selenium.dev/");
-		assertEquals(title, driver.getTitle());
+		assertEquals(title, driver.getTitle()); // add AssertJ
 	}
 	
 	@AfterEach
